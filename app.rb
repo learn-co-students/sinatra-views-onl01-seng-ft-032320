21fab27901848ad6044d6462c50b23e8f8cb1c88
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-28 17:26:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index # pulls the erb file inside of views folder and renders that HTML
 	end
+
+	get '/info' do
+	 erb :info
+	end
+
+
 end
